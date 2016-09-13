c3f6dbca3d689608fe0b5027467faac8dc8aa1d8
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-09-13 22:44:54 +0200

    Adding startup script for creating table and add user

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Login_1DV608
 Interface repository for 1DV608 assignment 2 and 4
+
+## Setup development environment
+* Install docker
+* run `docker-compose up`
+* run bash for php container `docker exec -it 1dv610assignment2_php_1 bash`
+* execute setup file `php createAndPopulateTables.php`
---
+++ src/createAndPopulateTables.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('./config.php');
+
+$setupUserTableQuery = "
+CREATE TABLE users (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(30) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  create_date TIMESTAMP
+);
+";
+
+$addAdminUserQuery = "
+INSERT INTO users (username, password) 
+VALUES(:username, :password);
+";
+
+$credentials = array(
+    'username' => 'Admin',
+    'password' => password_hash('Password', PASSWORD_DEFAULT)
+);
+
+$db->exec($setupUserTableQuery);
+$db->prepare($addAdminUserQuery)->execute($credentials);
